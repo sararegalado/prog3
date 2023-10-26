855f8fd55bb30f5b61fc4e0dfff8a72b43dab46d
sararegalado <144669342+sararegalado@users.noreply.github.com>
2023-10-26 10:11:12 +0200

    Add files via upload
    PRACTICA6

---
+++ practica6/Ejercicio06_03.java
@@ -0,0 +1,53 @@
+package cap06.practica6;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Ejercicio06_03 {
+	
+	private static JFrame ventana;
+	private static DataSetMunicipios dataset;
+
+	private static VentanaMunicipios ventanaDatos;
+	
+	public static void main(String[] args) {
+		ventana = new JFrame( "Ejercicio 6.3" );
+		ventana.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		ventana.setLocationRelativeTo( null );
+		ventana.setSize( 200, 80 );
+
+		JButton bCargaMunicipios = new JButton( "Carga municipios > 50k" );
+		ventana.add( bCargaMunicipios );
+		
+		bCargaMunicipios.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cargaMunicipios();
+			}
+		});
+		
+		ventana.setVisible( true );
+	}
+	
+	private static void cargaMunicipios() {
+		try {
+			dataset = new DataSetMunicipios( "municipios50k.txt" );
+			System.out.println( "Cargados municipios:" );
+			for (Municipio m : dataset.getListaMunicipios() ) {
+				System.out.println( "\t" + m );
+			}
+			
+			ventanaDatos = new VentanaMunicipios();
+			ventanaDatos.setDatosArbol( dataset );
+			ventanaDatos.setVisible( true );
+
+		} catch (IOException e) {
+			System.err.println( "Error en carga de municipios" );
+		}
+	}
+	
+}
